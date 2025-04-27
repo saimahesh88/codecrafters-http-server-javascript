a71ea419de9c33479d3fa53b04f4fa83e61f81fd
Mahesh <nsaimahesh8@gmail.com>
2025-04-27 20:59:40 +0530

    send response based on url path

--- app/main.js
+++ app/main.js
@@ -5,10 +5,19 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n")
-  socket.on("close", () => {
+  socket.on("data",(data) => {
+    let requestBody = data.toString();
+    console.log('requestBody '+requestBody.toString());
+    const lines = requestBody.split("\r\n");
+    const [method, path] = lines[0].split(" ");
+    if(method == "GET" && path == "/"){
+        socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    }
+    else{
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    }
     socket.end();
-  });
+  })
 });
 
 server.listen(4221, "localhost");
