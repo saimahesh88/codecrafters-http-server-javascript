7f03a768baa2e45848ba3ae45a1b7252eb3ff73e
Mahesh <nsaimahesh8@gmail.com>
2025-04-28 00:45:51 +0530

    get user agent

--- app/main.js
+++ app/main.js
@@ -9,6 +9,13 @@ const server = net.createServer((socket) => {
     let requestBody = data.toString();
     const lines = requestBody.split("\r\n");
     const [method, path] = lines[0].split(" ");
+    let headers = {};
+    for (let i = 1; i < lines.length; i++) {
+        let headerLine = lines[i].split(": ");
+        if (headerLine.length === 2) {
+            headers[headerLine[0]] = headerLine[1];
+        }
+    }
     if(method == "GET" && path == "/"){
         socket.write("HTTP/1.1 200 OK\r\n\r\n")
     }
@@ -19,6 +26,16 @@ const server = net.createServer((socket) => {
       `Content-Length: ${toEcho.length}\r\n` +
       `\r\n` + 
       `${toEcho}`// Important: Empty line separating headers and body
+      socket.write(response)
+    }
+    else if(method == "GET" && path.includes("/user-agent")){
+      let userAgent = headers['User-Agent'] || '';
+      console.log('userAgent '+userAgent)
+      const response = `HTTP/1.1 200 OK\r\n` +
+      `Content-Type: text/plain\r\n` +
+      `Content-Length: ${userAgent.length}\r\n` +
+      `\r\n` +
+      `${userAgent}`
       socket.write(response)
     }
     else{
