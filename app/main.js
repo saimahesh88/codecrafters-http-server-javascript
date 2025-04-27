88815f035a8c6da35782ae5621dd79c4eca25ea4
Mahesh <nsaimahesh8@gmail.com>
2025-04-28 00:12:54 +0530

    response with body

--- app/main.js
+++ app/main.js
@@ -5,13 +5,21 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data",(data) => {
+  socket.on("data",(data, callback) => {
     let requestBody = data.toString();
-    console.log('requestBody '+requestBody.toString());
     const lines = requestBody.split("\r\n");
     const [method, path] = lines[0].split(" ");
     if(method == "GET" && path == "/"){
         socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    }
+    else if(method == "GET" && path.includes("/echo")){
+      const toEcho = path.substring(6)
+      const response = `HTTP/1.1 200 OK\r\n` +
+      `Content-Type: text/plain\r\n` +
+      `Content-Length: ${toEcho.length}\r\n` +
+      `\r\n` + 
+      `${toEcho}`// Important: Empty line separating headers and body
+      socket.write(response)
     }
     else{
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
